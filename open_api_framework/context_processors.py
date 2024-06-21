cd2f2eb99c379049af57fbc92424b50b05d734a7
Sonny Bakker <sonny871@hotmail.com>
2024-06-21 14:23:08 +0200

    [#21] use `is_staff` to determine if version is shown

--- open_api_framework/context_processors.py
+++ open_api_framework/context_processors.py
@@ -3,23 +3,7 @@ from django.http.request import HttpRequest
 
 
 def admin_settings(request: HttpRequest) -> dict:
-    resolver = request.resolver_match
-    show_version = False
-
-    if resolver:
-        if resolver.app_name:
-            url_name = f"{resolver.app_name}:{resolver.url_name}"
-        else:
-            url_name = resolver.url_name
-
-        # Note that only views that use the open-api-framework's
-        # `base_site.html` template are listed here
-        excluded_version_urls = [
-            "admin_password_reset",
-            "password_reset_done",
-        ]
-
-        show_version = bool(url_name not in excluded_version_urls)
+    show_version = request.user.is_staff
 
     return {
         "show_environment": getattr(settings, "ENVIRONMENT_SHOWN_IN_ADMIN", None),
