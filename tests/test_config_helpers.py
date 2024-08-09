c3387c5117d6e3a4215488d6d10017c17490a06e
Steven Bal <steven@maykinmedia.nl>
2024-08-09 16:33:13 +0200

    :white_check_mark: [open-zaak/open-zaak#1649] Tests for generate envvar docs

--- tests/test_config_helpers.py
+++ tests/test_config_helpers.py
@@ -2,12 +2,12 @@ from open_api_framework.conf.utils import config
 
 
 def test_empty_list_as_default():
-    value = config("SOME_TEST_ENVVAR", split=True, default=[])
+    value = config("SOME_TEST_ENVVAR", split=True, default=[], add_to_docs=False)
 
     assert value == []
 
 
 def test_non_empty_list_as_default():
-    value = config("SOME_TEST_ENVVAR", split=True, default=["foo"])
+    value = config("SOME_TEST_ENVVAR", split=True, default=["foo"], add_to_docs=False)
 
     assert value == ["foo"]
