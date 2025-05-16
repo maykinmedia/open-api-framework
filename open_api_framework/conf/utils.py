99502fd40b5e7ebba84f868bcf962effbce469d3
Steven Bal <steven@maykinmedia.nl>
2025-05-16 16:03:10 +0200

    :sparkles: Add mute_logging util for CI

--- open_api_framework/conf/utils.py
+++ open_api_framework/conf/utils.py
@@ -135,3 +135,24 @@ def get_django_project_dir() -> str:
     # Get the path of the importing module
     base_dirname = get_project_dirname()
     return Path(sys.modules[base_dirname].__file__).parent
+
+
+def mute_logging(config: dict) -> None:  # pragma: no cover
+    """
+    Disable (console) output from logging.
+    :arg config: The logging config, typically the django LOGGING setting.
+    """
+
+    # set up the null handler for all loggers so that nothing gets emitted
+    for name, logger in config["loggers"].items():
+        if name == "flaky_tests":
+            continue
+        logger["handlers"] = ["null"]
+
+    # some tooling logs to a logger which isn't defined, and that ends up in the root
+    # logger -> add one so that we can mute that output too.
+    config["loggers"].update(
+        {
+            "": {"handlers": ["null"], "level": "CRITICAL", "propagate": False},
+        }
+    )
